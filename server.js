88bb843a1f3c6973de038895809d6df00aa7f0b8
Vy Nguyen <vy1872@gmail.com>
2022-08-25 16:19:05 -0700

    added more files needed and got server to run

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require(`express`);
+const api = require(`./routes/index`)
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(`/api`, api)
+
+app.listen(PORT, () => console.log(`App listening at ${PORT}`));
