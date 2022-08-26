5fb7b747ff7658174f55833dd64e0ba7a767160b
Vy Nguyen <vy1872@gmail.com>
2022-08-25 21:29:28 -0700

    couldn't use the back tick

--- db/schema.sql
+++ db/schema.sql
@@ -24,4 +24,4 @@ CREATE TABLE employees (
     manager_id INT,
     FOREIGN KEY (role_id) REFERENCES roles(id) ON DELETE SET NULL,
     FOREIGN KEY (manager_id) REFERENCES employees(id) ON DELETE SET NULL
-)
+);
