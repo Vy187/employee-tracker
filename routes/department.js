dd7fb697d25bfcb2573079affa04e513bfeff7f5
Vy Nguyen <vy1872@gmail.com>
2022-08-26 23:09:53 -0700

    got the roles routes working and took the put out

--- routes/department.js
+++ routes/department.js
@@ -2,7 +2,7 @@ const department = require(`express`).Router();
 const db = require(`../config/connection`);
 
 department.get(`/`, (req, res) => {
-    db.query(`SELECT id, department_name AS title FROM departments`, (err, rows) => {
+    db.query(`SELECT id, department_name FROM departments`, (err, rows) => {
         (err) ? res.status(500).json({ error: err.message }) : res.json({ message: `success`, data: rows});
     })
 })
@@ -10,18 +10,6 @@ department.get(`/`, (req, res) => {
 department.post(`/new-department`, ( { body }, res) => {
     db.query(`INSERT INTO departments (department_name) VALUES (?)`, [body.department_name], (err, result) => {
         (err) ? res.status(400).json({ error: err.message}) : res.json({ message: `success`, data: body});
-    })
-})
-
-department.put(`/:id`, (req, res) => {
-    db.query(`UPDATE departments SET department_name = ? WHERE id = ?`, [req.body.department_name, req.params.id], (err, result) => {
-        if (err) {
-            res.status(400).json({ error: res.message });
-        } else if (!result.affectedRows) {
-            res.json({ message: `Department not found`});
-        } else {
-            res.json({ message: `updated`, data: req.body, changes: result.affectedRows});
-        }
     })
 })
 
--- routes/role.js
+++ routes/role.js
@@ -1,4 +1,28 @@
 const role = require(`express`).Router();
 const db = require(`../config/connection`);
 
+role.get(`/`, (req, res) => {
+    db.query(`SELECT roles.id, roles.title, roles.salary, departments.department_name FROM roles INNER JOIN departments ON roles.department_id = departments.id`, (err, rows) => {
+        (err) ? res.status(500).json({ error: err.message }) : res.json({ message: `success`, data: rows});
+    })
+})
+
+role.post(`/new-role`, ( { body }, res) => {
+    db.query(`INSERT INTO roles (title, salary, department_id) VALUES (?, ?, ?)`, [body.title, body.salary, body.department_id], (err, result) => {
+        (err) ? res.status(400).json({ error: err.message}) : res.json({ message: `success`, data: body});
+    })
+})
+
+role.delete(`/:id`, (req, res) => {
+    db.query(`DELETE FROM roles WHERE id = ?`, [req.params.id], (err, result) => {
+        if (err) {
+            res.statusMessage(400).json({ error: res.message });
+        } else if (!result.affectedRows) {
+            res.json({ message: `Role not found`});
+        } else {
+            res.json({ message: `deleted`, changes: result.affectedRows, id: req.params.id});
+        }
+    })
+})
+
 module.exports = role;
