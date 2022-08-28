4d830b07ec556c4d84c6bc72eca46d896a3f0b3c
Vy Nguyen <vy1872@gmail.com>
2022-08-28 12:54:17 -0700

    add node-fetch package so I can fetch data through the backend and now you are able to see all departments, roles, and employees

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,8 +1,24 @@
 const inquier = require(`inquirer`);
+const fetch = require(`node-fetch`)
 const cTable = require(`console.table`);
 
-const app = () => {
-    inquier.prompt({
+const getData = (selection) =>
+    fetch(`http://localhost:3001/api/${selection}/`, {
+        method: `GET`,
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+
+const renderData = async (results) => {
+    const data = await results.json();
+    console.table(data.data)    
+}
+
+const getAndRenderData = (selection) => getData(selection).then(renderData).then(app);
+
+const app = async () => {
+    await inquier.prompt({
         type: `list`,
         name: `choice`,
         message: `What would you like to do?`,
@@ -10,7 +26,7 @@ const app = () => {
     }).then((answer) => {
         switch (answer.choice) {
             case `View All Employees`:
-
+                getAndRenderData(`employee`);
                 break;
             case `Add Employee`:
 
@@ -19,13 +35,13 @@ const app = () => {
 
                 break;
             case `View All Roles`:
-
+                getAndRenderData(`role`);
                 break;
             case `Add Role`:
 
                 break;
             case `View All Departments`:
-
+                getAndRenderData(`department`);
                 break;
             case `Add Departments`:
 
